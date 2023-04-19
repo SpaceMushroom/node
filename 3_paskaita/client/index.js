89fd5093834ff188a9f2ceb724e9548e10a94eb4
Ignas Grinius <igrinius@gmail.com>
2023-04-19 21:36:44 +0300

    index.js update

--- 3_paskaita/client/index.js
+++ 3_paskaita/client/index.js
@@ -4,27 +4,13 @@ fetch("http://localhost:3000/users")
         const table = document.querySelector('tbody');
         response.forEach((user) => {
             const tr = document.createElement('tr');
-            const pas = document.createElement('td');
-            pas.textContent = user.pas;
-            const pass = document.createElement('td');
-            pass.textContent = user.pass;
-            const email = document.createElement('td');
-            email.textContent = user.email;
-            const name = document.createElement('td');
-            name.textContent = user.name;
-            const surname = document.createElement('td');
-            surname.textContent = user.surname;
-            const adress = document.createElement('td');
-            adress.textContent = user.adress;
-            const zip = document.createElement('td');
-            zip.textContent = user.zip;
-            const city = document.createElement('td');
-            city.textContent = user.city;
-            const phone = document.createElement('td');
-            phone.textContent = user.phone;
-            const agree = document.createElement('td');
-            agree.textContent = user.agree;
-            tr.append(pas, pass, email, name, surname, adress, zip, city, phone, agree)
-            table.append(tr);
-          });
-    })
+            Object.values(user).forEach((value) => {
+                const td = document.createElement("td");
+                td.textContent = value;
+                tr.append(td);
+                });
+                table.append(tr);
+        });
+        table.append(tr);
+    });
+    
